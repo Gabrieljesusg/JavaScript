74d6a3356aa197758b96143c800533217a5445ff
Gabrieljesusg <135985907+Gabrieljesusg@users.noreply.github.com>
2024-02-06 08:56:12 -0300

    incluindo exercicios de verificação de dados

--- aula13 exercicios/script.js
+++ aula13 exercicios/script.js
@@ -3,7 +3,46 @@ function verificar(){
   var ano = data.getFullYear()
   var fano = document.getElementById("txtano")
   var res = document.getElementById('res')
-  if (fano.value.length == 0 || fano.value > ano){
+  if (fano.value.length == 0 || Number(fano.value) > ano){
     alert('[ERRO] Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked){
+      genero = 'Homem'
+      if (idade >= 10 && idade < 10){
+       img.setAttribute('src', 'assets/homemcrianca.jpg')  
+      } else if (idade < 21){
+        //jovem
+        img.setAttribute('src', 'assets/homemjovem.jpg')
+      } else if (idade < 50){
+        //adulto
+        img.setAttribute('src', 'assets/homemadulto.jpg')
+      } else {
+        //idoso
+        img.setAttribute('src', 'assets/homemvelho.jpg')
+      }
+    } else if (fsex[1].checked){
+      genero = 'Mulher'
+      if (idade >= 10 && idade <10){
+        //criança
+        img.setAttribute('src', 'assets/mulhercrianca.jpg')
+      } else if (idade < 21){
+        // jovem
+        img.setAttribute('src', 'assets/mulherjovem.jpg')
+      } else if (idade < 50) {
+        // adulto
+        img.setAttribute('src', 'assets/mulheradulta.jpg')
+      } else {
+        //idoso
+        img.setAttribute('src', 'assets/mulhervelha.jpg')
+      }
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
+    
   }
 }
